adcf6ed9186c152ab206604c88642efd012bc2e3
PrinceBaghel25 <112877490+PrinceBaghel25@users.noreply.github.com>
2023-01-18 21:59:05 +0530

    Add files via upload

---
+++ Area_Of_A_Circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define pi 3.14
+main()
+{
+    float r,area;
+    r=5.6;
+    area=pi*r*r;
+    printf("Radius of a circle is %f\n",r);
+    printf("Area of a circle is %f",area);
+}
---
+++ abs.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter an negative value : ";
+    cin>>n;
+    cout<<abs(n);
+    return 0;
+}
